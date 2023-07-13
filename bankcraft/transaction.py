b5ad70e2e58c1de2f6089c9adbc9f74b7a410fa1
Razyeh Behbahani <rbehbahani@mun.ca>
2023-07-13 10:40:45 -0400

    boolean check for txn_type_July13

--- bankcraft/transaction.py
+++ bankcraft/transaction.py
@@ -7,19 +7,12 @@ class Transaction:
         self.amount = amount
         self.date_of_transaction = date
         self._txn_type = txn_type
-        self.check_txn_type()
 
     def do_transaction(self):
-        self.check_txn_type()
-        if self.sender_account is not None:
-            if self.sender_account.balance >= self.amount:
-                self.sender_account.balance -= self.amount
-                if self.recipient_account is not None:
-                    self.recipient_account.balance += self.amount
-            else:
-                # print("txn_amount is more than account balance!")
-                pass
+        if self.txn_type_is_defined():
+            self.sender_account.balance -= self.amount
+            if self.recipient_account is not None:
+                self.recipient_account.balance += self.amount
 
-    def check_txn_type(self):
-        if str(self._txn_type).lower() not in ["cash", "wire", "online", "ach", "cheque"]:
-            raise ValueError("Undefined txn type")
+    def txn_type_is_defined(self):
+        return str(self._txn_type).lower() in ["cash", "wire", "online", "ach", "cheque"]
